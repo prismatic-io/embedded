63a206500bed9795babb2bb32682ddf21b537080
Jason Comes <jasonwcomes@gmail.com>
2023-03-28 15:04:41 -0500

    Add all listing page translations

--- src/types/phrases.ts
+++ src/types/phrases.ts
@@ -718,6 +718,133 @@ interface SharedPhrases {
   "noResults.text": ComplexPhrase<{ searchTerm: string }>;
 
   // listing pages empty state
+
+  /** English: "Alert groups" */
+  "emptyState.alertGroupsTitle": SimplePhrase;
+
+  /** English: "You likely want to be notified when an instance deployed to a customer does something out of the ordinary (maybe it throws an error because a third-party API is down, or is disabled by a customer unexpectedly)." */
+  "emptyState.alertGroupsText": SimplePhrase;
+
+  /** English: "You can create groups of people who should be notified when an alert monitor on an instance is triggered." */
+  "emptyState.alertGroupsTextTwo": SimplePhrase;
+
+  /** English: "Learn about alert groups" */
+  "emptyState.alertGroupsDocsButton": SimplePhrase;
+
+  /** English: "Alert monitors" */
+  "emptyState.alertMonitorsTitle": SimplePhrase;
+
+  /** English: "If an instance encounters an error (maybe a third-party API is down), you probably want to be alerted. You can configure monitoring and alerting rules to notify your team or even customers via text, email, or webhook when certain events occur." */
+  "emptyState.alertMonitorsText": SimplePhrase;
+
+  /** English: "Not all alerts are bad - you can also alert people when an instance is enabled, or when it runs successfully." */
+  "emptyState.alertMonitorsTextTwo": SimplePhrase;
+
+  /** English: "Here you will see any alerts that have been configured for you integrations." */
+  "emptyState.alertMonitorsText--customer": SimplePhrase;
+
+  /** English: "Learn about alert monitors" */
+  "emptyState.alertMonitorsDocsButton": SimplePhrase;
+
+  /** English: "Alert webhooks" */
+  "emptyState.alertWebhooksTitle": SimplePhrase;
+
+  /** English: "Alert webhooks allow you to send alert notifications to a third-party monitoring system, like PagerDuty, OpsGenie, or Slack, or to a webhook endpoint that you manage." */
+  "emptyState.alertWebhooksText": SimplePhrase;
+
+  /** English: "Learn about alert webhooks" */
+  "emptyState.alertWebhooksDocsButton": SimplePhrase;
+
+  /** English: "Customer attachments" */
+  "emptyState.attachmentsTitle": SimplePhrase;
+
+  /** English: "Attachments are a place to store documents that both customers and your team members can reference." */
+  "emptyState.attachmentsText": SimplePhrase;
+
+  /** English: "Organization credential" */
+  "emptyState.credentialsTitle": SimplePhrase;
+
+  /** English: "Credentials that are saved at an organization level are either applicable to all of your customers (like a shared API key), or they're used by your team members for testing integrations." */
+  "emptyState.credentialsText": SimplePhrase;
+
+  /** English: "Learn About Credentials" */
+  "emptyState.credentialsDocsButton": SimplePhrase;
+
+  /** English: "OAuth 2.0 in Prismatic" */
+  "emptyState.credentialsDocsButtonTwo": SimplePhrase;
+
+  /** English: "Customer credentials" */
+  "emptyState.customerCredentialsTitle": SimplePhrase;
+
+  /** English: "When you deploy an instance of an integration to a customer, that customer's credentials are used to connect to third-party apps and services." */
+  "emptyState.customerCredentialsText": SimplePhrase;
+
+  /** English: "Customers" */
+  "emptyState.customersTitle": SimplePhrase;
+
+  /** English: "You'll manage your customers here. You can deploy any number of your integrations to each customer, or they can activate integrations themselves through the marketplace." */
+  "emptyState.customersText": SimplePhrase;
+
+  /** English: "From each customer record, you can deploy or reconfigure their integrations, review logs, manage their users, and more." */
+  "emptyState.customersTextTwo": SimplePhrase;
+
+  /** English: "Learn about customers" */
+  "emptyState.customersDocsButton": SimplePhrase;
+
+  /** English: "Customer users" */
+  "emptyState.customerUsersTitle": SimplePhrase;
+
+  /** English: "You can invite your customers to log in to Prismatic, so they can manage instances deployed to them and review their own executions and logs." */
+  "emptyState.customerUsersText": SimplePhrase;
+
+  /** English: "Note: You do not need to add customer users to Prismatic if your users authenticate through an embedded marketplace." */
+  "emptyState.customerUsersTextTwo": SimplePhrase;
+
+  /** English: "Learn about customer users" */
+  "emptyState.customerUsersDocsButton": SimplePhrase;
+
+  /** English: "Embedded marketplace user authentication" */
+  "emptyState.customerUsersDocsButtonTwo": SimplePhrase;
+
+  /** English: "Instance executions" */
+  "emptyState.executionsTitle": SimplePhrase;
+
+  /** English: "Each time an instance of an integration runs, that's called an execution. Execution details are available here." */
+  "emptyState.executionsText": SimplePhrase;
+
+  /** English: "You can inspect execution logs and results of each step of the execution, which is useful for support and debugging purposes." */
+  "emptyState.executionsTextTwo": SimplePhrase;
+
+  /** English: "Learn about instance executions" */
+  "emptyState.executionsDocsButton": SimplePhrase;
+
+  /** English: "Instances" */
+  "emptyState.instancesTitle": SimplePhrase;
+
+  /** English: "In Prismatic, integrations are reusable - they can be deployed to multiple customers and configured to behave differently from one customer to the next." */
+  "emptyState.instancesText": SimplePhrase;
+
+  /** English: "An instance is an integration that has been deployed to a specific customer using customer-specific configuration." */
+  "emptyState.instancesTextTwo": SimplePhrase;
+
+  /** English: "Here you will see any integrations that have been configured in the marketplace or deployed directly to your organization." */
+  "emptyState.instancesText--customer": SimplePhrase;
+
+  /** English: "Learn about instances" */
+  "emptyState.instancesDocsButton": SimplePhrase;
+
+  /** English: "Integrations" */
+  "emptyState.integrationsTitle": SimplePhrase;
+
+  /** English: "Integrations are reusable workflows that connect your product to the other apps and services your customers use." */
+  "emptyState.integrationsText": SimplePhrase;
+
+  /** English: "After building an integration, you can deploy it to one or more of your customers, or add it to your marketplace, where customers can activate it themselves." */
+  "emptyState.integrationsTextTwo": SimplePhrase;
+
+  /** English: "Learn about integrations" */
+  "emptyState.integrationsDocsButton": SimplePhrase;
+
   /** English: "Integration marketplace" */
   "emptyState.integrationMarketplaceTitle": SimplePhrase;
 
@@ -727,11 +854,41 @@ interface SharedPhrases {
   /** English: "Here is where you set up which integrations are included in your marketplace and how they appear." */
   "emptyState.integrationMarketplaceTextTwo": SimplePhrase;
 
+  /** English: "The integration marketplace displays integrations that have been enabled or available for configuration." */
+  "emptyState.integrationMarketplaceText--customer": SimplePhrase;
+
   /** English: "Learn about the integration marketplace" */
   "emptyState.integrationMarketplaceDocsButton": SimplePhrase;
 
   /** English: "Embedding marketplace in your app" */
   "emptyState.integrationMarketplaceDocsButtonTwo": SimplePhrase;
+
+  /** English: "Logs" */
+  "emptyState.logsTitle": SimplePhrase;
+
+  /** English: "If actions in your deployed instances generate log messages, you'll find those here." */
+  "emptyState.logsText": SimplePhrase;
+
+  /** English: "From this page you can search and filter logs so you can keep tabs on exactly what's going on with your customers' live integrations." */
+  "emptyState.logsTextTwo": SimplePhrase;
+
+  /** English: "If actions in your deployed instances generate log messages, you'll find those here." */
+  "emptyState.logsText--customer": SimplePhrase;
+
+  /** English: "From this page you can search and filter logs so you can keep tabs on exactly what's going on with your integrations." */
+  "emptyState.logsTextTwo--customer": SimplePhrase;
+
+  /** English: "Learn about logs" */
+  "emptyState.logsDocsButton": SimplePhrase;
+
+  /** English: "Log streams" */
+  "emptyState.logStreamsTitle": SimplePhrase;
+
+  /** English: "Log streams allow you to send Prismatic logs to an external logging service (like DataDog or New Relic) or your own proprietary logging system." */
+  "emptyState.logStreamsText": SimplePhrase;
+
+  /** English: "Learn about log streams" */
+  "emptyState.logStreamsDocsButton": SimplePhrase;
 }
 
 export interface UniquePhrases {
