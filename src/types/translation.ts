29953d24ba6903705c15e1b6fcc31a33a9d0688e
awburgard <austin.burgard@prismatic.io>
2024-01-16 15:08:55 -0600

    Update Translation interface to include dynamicPhrase property
    [sc-17872]

--- src/types/translation.ts
+++ src/types/translation.ts
@@ -1,6 +1,10 @@
 import { Phrases } from "@prismatic-io/translations";
 
+interface EmdeddedPhrases extends Phrases {
+  dynamicPhrase: { [x: string]: string };
+}
+
 export interface Translation {
   debugMode?: boolean;
-  phrases?: Phrases;
+  phrases?: EmdeddedPhrases;
 }
