688ebfeb55502327e4a3680c0db0c267207c86a5
Austin Burgard <awburgard@gmail.com>
2024-01-17 09:41:11 -0600

    Removes Dynamic Phrases  (#62)
    * removes types
    
    * bumps translation version

--- src/types/translation.ts
+++ src/types/translation.ts
@@ -1,10 +1,6 @@
 import { Phrases } from "@prismatic-io/translations";
 
-interface EmdeddedPhrases extends Phrases {
-  dynamicPhrase: { [x: string]: string };
-}
-
 export interface Translation {
   debugMode?: boolean;
-  phrases?: EmdeddedPhrases;
+  phrases?: Phrases;
 }
