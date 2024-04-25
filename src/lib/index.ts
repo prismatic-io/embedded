c28447b9f93e30dbb97a354ac57d94535a8b8595
Jason Comes <jasonwcomes@gmail.com>
2024-04-25 13:51:09 -0500

    Configure Instance - Add `integrationid, instanceId` props to method

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,9 @@
-import { configureInstance } from "./configureInstance";
+import {
+  configureInstance,
+  isConfigureInstanceWithInstanceId,
+  isConfigureInstanceWithIntegrationId,
+  isConfigureInstanceWithIntegrationName,
+} from "./configureInstance";
 
 export { authenticate } from "./authenticate";
 export {
@@ -7,6 +12,9 @@ export {
    * @deprecated Use configureInstance instead, this will be removed in the next major version (v3.0.0)
    */
   configureInstance as configureIntegration, // alias for backwards compatibility
+  isConfigureInstanceWithInstanceId,
+  isConfigureInstanceWithIntegrationId,
+  isConfigureInstanceWithIntegrationName,
 };
 export { graphqlRequest } from "./graphqlRequest";
 export { init, EMBEDDED_DEFAULTS } from "./init";
